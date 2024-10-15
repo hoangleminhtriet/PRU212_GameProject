0f829642352463216bc2dd7313cedcc39e24e755
Bùi Gia Bảo <Baobgce170974@fpt.edu.vn>
2024-10-15 08:38:55 +0700

    UpdateMenu

---
+++ Assets/Scripts/Settings.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Settings : MonoBehaviour
+{
+	public AudioMixer audioMixer;
+	public Slider volumeSlider;
+
+	void Start()
+	{
+		// Đặt giá trị ban đầu của slider
+		volumeSlider.value = PlayerPrefs.GetFloat("Volume", 0.75f);
+	
+	}
+
+	public void SetVolume(float volume)
+	{
+		audioMixer.SetFloat("Volume", Mathf.Log10(volume) * 20); // Logarithmic scale cho âm lượng
+		PlayerPrefs.SetFloat("Volume", volume); // Lưu lại giá trị âm lượng
+	}
+
+	public void BackToMainMenu()
+	{
+		SceneManager.LoadScene("MainMenu");
+	}
+}
