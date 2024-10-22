7516b4e7f65191f34cd70c4a81846829a74b23d9
hoangleminhtriet.bonzy <hoangleminhtriet@gmail.com>
2024-10-22 12:29:53 +0700

    load scene

--- Assets/Scripts/SceneChanger.cs
+++ Assets/Scripts/SceneChanger.cs
@@ -4,16 +4,20 @@ using UnityEngine.SceneManagement;
 public class SceneChanger2D : MonoBehaviour
 {
 	// Tên của scene bạn muốn load
-	public string Map2;
+	public string SceneName;
 
 	// Hàm này sẽ được gọi khi một đối tượng khác va chạm với Trigger Collider 2D
+	public void LoadSceneLevel()
+	{
+		SceneManager.LoadScene(SceneName);
+	}
 	private void OnTriggerEnter2D(Collider2D other)
 	{
 		// Kiểm tra nếu đối tượng va chạm là nhân vật của bạn
 		if (other.CompareTag("Player"))
 		{
 			// Chuyển scene
-			SceneManager.LoadScene(Map2);
+			LoadSceneLevel();
 		}
 	}
 }
