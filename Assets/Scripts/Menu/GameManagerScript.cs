941164218211337a53d80a7f0535f463aa012761
minh.nhat.16 <nguyennhatminh.16.it@gmail.com>
2024-10-28 22:30:27 +0700

    up date last minh

--- Assets/Scripts/Menu/GameManagerScript.cs
+++ Assets/Scripts/Menu/GameManagerScript.cs
@@ -1,31 +1,28 @@
-<<<<<<< Updated upstream
+/*<<<<<<< Updated upstream
 using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering.UI;
-=======
+=======*/
+using Cinemachine;
 using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
->>>>>>> Stashed changes
 using UnityEngine.UI;
 
 public class GameManagerScript : MonoBehaviour
 {
 	public static GameManagerScript instance;
-<<<<<<< Updated upstream
 	public CinemachineVirtualCamera cameChine;
 	public Playercontroller playerController;
 	public List<Vector3> loadMapPos = new List<Vector3>();
 	public int currentLevel = 1;
     public Text MessageOpen;
     public Text newMap;
-=======
-	public Text doorOpen;
-	public Text newMap;
+
 	public UnityEvent newMapEvent =  new();
     private void OnEnable()
     {
@@ -33,8 +30,6 @@ public class GameManagerScript : MonoBehaviour
     }
 
 
-
->>>>>>> Stashed changes
     private void Awake()
 	{
 		instance = this;
@@ -48,6 +43,7 @@ public class GameManagerScript : MonoBehaviour
     public IEnumerator TurnOffMessage()
     {
         yield return new WaitForSeconds(3);
+        Debug.LogWarning("inactive message");
         newMap.gameObject.SetActive(false);
     }
 }
--- Assets/Scripts/Menu/SceneChanger.cs
+++ Assets/Scripts/Menu/SceneChanger.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.UI;
 
@@ -12,20 +11,23 @@ public class SceneChanger2D : MonoBehaviour
     public int targetEnemy;
     public bool isDoorOpen = false;
 
-<<<<<<< Updated upstream
-	public UnityEvent<bool > isDoorCanOpen = new UnityEvent<bool> ();
-	public UnityEvent<int> totalEnemyKill = new UnityEvent<int> ();
-
-	public GameManagerScript gameManagerScript;
-=======
     public UnityEvent<bool> isDoorCanOpen = new UnityEvent<bool>();
     public UnityEvent<int> totalEnemyKill = new UnityEvent<int>();
+
+    public GameManagerScript gameManagerScript;
     public Text MessageOpen;
     public Text newMap;
     // Hàm này sẽ được gọi khi một đối tượng khác va chạm với Trigger Collider 2D
-    private void OnTriggerEnter2D(Collider2D other)
+
+    private void OnEnable()
     {
 
+        isDoorCanOpen.AddListener(OpenDoor);
+        totalEnemyKill.AddListener(KillEnemy);
+    }
+    // Hàm này sẽ được gọi khi một đối tượng khác va chạm với Trigger Collider 2D
+    private void OnTriggerEnter2D(Collider2D other)
+    {
         // Kiểm tra nếu đối tượng va chạm là nhân vật của bạn
         if (other.CompareTag("Player") && isDoorOpen)
         {
@@ -33,76 +35,25 @@ public class SceneChanger2D : MonoBehaviour
             // Chuyển scene
             LoadSceneManager.instance.LoadSceneByName(SceneLevel, () =>
             {
-                newMap.text = "Arrive to new map " + SceneLevel;
-                newMap.gameObject.SetActive(true);
-                MessageOpen.gameObject.SetActive(false);
-                Debug.Log("Loadddddddd" + newMap.isActiveAndEnabled);
-                GameManagerScript.instance.newMapEvent?.Invoke();
->>>>>>> Stashed changes
+                var gameManager = GameManagerScript.instance;
 
+                int currentLevel = GameManagerScript.instance.currentLevel;
+                gameManager.newMap.text = "Arrive to new map " + SceneLevel;
+                gameManager.newMap.gameObject.SetActive(true);
+                gameManager.MessageOpen.gameObject.SetActive(false);
+                gameManager.newMapEvent.Invoke();
             });
         }
     }
-    private void OnEnable()
+    private void Start()
     {
-        
-        isDoorCanOpen.AddListener(OpenDoor);
-        totalEnemyKill.AddListener(KillEnemy);
-
-<<<<<<< Updated upstream
-	// Hàm này sẽ được gọi khi một đối tượng khác va chạm với Trigger Collider 2D
-	private void OnTriggerEnter2D(Collider2D other)
-	{
-		// Kiểm tra nếu đối tượng va chạm là nhân vật của bạn
-		if (other.CompareTag("Player") && isDoorOpen)
-		{
-			Debug.Log("Player enter");
-            // Chuyển scene
-            LoadSceneManager.instance.LoadSceneByName(SceneLevel, () =>
-			{
-				var gameManager = GameManagerScript.instance;
-
-                int currentLevel = GameManagerScript.instance.currentLevel;
-                gameManager.newMap.text = "Arrive to new map" + SceneLevel;
-                gameManager.newMap.gameObject.SetActive(true);
-                gameManager.MessageOpen.gameObject.SetActive(false);
-				Vector3 playerPos = GameManagerScript.instance.loadMapPos[currentLevel];
-				var player = GameManagerScript.instance.playerController;
-				player.transform.position = transform.position = playerPos;
-				Debug.Log("Loadddddddd"+ playerPos);
-			});
-		}
-	}
-	private void Start()
-	{
         targetEnemy = LevelControllerScript.instance.totalEnemy;
         isDoorCanOpen.AddListener(OpenDoor);
-		totalEnemyKill.AddListener(KillEnemy);
-		gameManagerScript = GameManagerScript.instance;
+        totalEnemyKill.AddListener(KillEnemy);
+        gameManagerScript = GameManagerScript.instance;
 
 
     }
-	private void KillEnemy(int currentEnemy)
-	{
-		isDoorCanOpen.Invoke(currentEnemy > 0);
-	}
-
-	private void OpenDoor(bool isOpen)
-	{
-		openDoor.SetActive (!isOpen);
-		closeDoor.SetActive (isOpen);
-		isDoorOpen = !isOpen;
-        GameManagerScript.instance.MessageOpen.gameObject.SetActive(!isOpen);
-	}
-	private void Update()
-	{
-		int currentEnemy = LevelControllerScript.instance.CurrentEnemy();
-		KillEnemy(currentEnemy);
-
-	}
-=======
-    }
- 
     private void KillEnemy(int currentEnemy)
     {
         isDoorCanOpen.Invoke(currentEnemy > 0);
@@ -110,23 +61,17 @@ public class SceneChanger2D : MonoBehaviour
 
     private void OpenDoor(bool isOpen)
     {
-        MessageOpen = GameManagerScript.instance.doorOpen;
-        newMap = GameManagerScript.instance.newMap;
         openDoor.SetActive(!isOpen);
         closeDoor.SetActive(isOpen);
         isDoorOpen = !isOpen;
-        MessageOpen.gameObject.SetActive(!isOpen);
-    }
-    private void Start()
-    {
-        targetEnemy = LevelControllerScript.instance.totalEnemy;
+        GameManagerScript.instance.MessageOpen.gameObject.SetActive(!isOpen);
     }
     private void Update()
     {
         int currentEnemy = LevelControllerScript.instance.CurrentEnemy();
         KillEnemy(currentEnemy);
+
     }
->>>>>>> Stashed changes
 
 }
 
