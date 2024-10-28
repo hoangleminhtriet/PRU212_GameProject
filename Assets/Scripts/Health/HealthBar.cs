e8f146a2fc824bee2a9d5ba38843008a19209378
Bùi Gia Bảo <Baobgce170974@fpt.edu.vn>
2024-10-28 13:38:24 +0700

    updatehealth

---
+++ Assets/Scripts/Health/HealthBar.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar  : MonoBehaviour
+{
+    private RectTransform bar;
+    private Image barImage;
+
+    // Start is called before the first frame update
+void Start()
+    {
+        bar = GetComponent<RectTransform>();
+        barImage = GetComponent<Image>();
+        if (Health.totalHealth < 0.3f)
+        {
+            barImage.color = Color.red;
+        }
+        SetSize(Health.totalHealth);
+    }
+
+    public void Damage(float damage)
+    {
+        Health.totalHealth -= damage; // Trừ máu một lần duy nhất
+
+        // Đảm bảo totalHealth không âm
+        if (Health.totalHealth < 0f)
+        {
+            Health.totalHealth = 0f;
+        }
+
+        if (Health.totalHealth < 0.3f)
+        {
+            barImage.color = Color.red;
+        }
+
+        SetSize(Health.totalHealth);
+    }
+
+    public void SetSize(float size)
+    {
+        bar.localScale = new Vector3(size, 1f, 1f);
+    }
+}
